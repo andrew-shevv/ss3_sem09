04e2ae3dc5e9fa99bb9f35c47ae8d882d9dbeae4
Andrew <vishnyaishere@gmail.com>
2022-09-10 19:32:51 +0300

    exc003 done

--- exc003/Program.cs
+++ exc003/Program.cs
@@ -3,3 +3,17 @@
 // и возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
+
+int DigitSum(int number)
+{
+    if (number / 10 == 0)
+    {
+        return number;
+    }
+    else return number % 10 + DigitSum(number / 10);
+}
+
+Console.Write("Input number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(DigitSum(num));
