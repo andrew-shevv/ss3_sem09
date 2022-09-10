f0b216ad19f5f4d1c091e1963d34899429857a84
Andrew <vishnyaishere@gmail.com>
2022-09-10 18:45:17 +0300

    exc001 done

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -3,3 +3,19 @@
 // в промежутке от 1 до N.
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void ListNumbers(int number)
+{
+    if (number > 0)
+    {
+        ListNumbers(number - 1);
+    }
+    else return;
+
+    Console.Write(number + " ");
+}
+
+Console.Write("Input number: ");
+int num = int.Parse(Console.ReadLine()!);
+
+ListNumbers(num);
