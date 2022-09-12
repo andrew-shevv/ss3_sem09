0c16e3205db638a560adaa350065592a6b715721
Andrew <vishnyaishere@gmail.com>
2022-09-12 20:55:52 +0300

    exc007 done

--- exc007_hw/Program.cs
+++ exc007_hw/Program.cs
@@ -2,3 +2,25 @@
 // вычисления функции Аккермана с помощью рекурсии. 
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
+
+int Ackermann(int firstNum, int secondNum)
+{
+    if (firstNum == 0)
+    {
+        return secondNum + 1;
+    }
+    else if (firstNum > 0 && secondNum == 0)
+    {
+        return Ackermann(firstNum - 1, 1);
+    }
+    else if (firstNum > 0 && secondNum > 0)
+    {
+        return Ackermann(firstNum - 1, Ackermann(firstNum, secondNum - 1));
+    }
+    else return 0;
+}
+
+const int FIRSTNUMBER = 2;
+const int SECONDNUMBER = 3;
+
+Console.Write(Ackermann(FIRSTNUMBER, SECONDNUMBER));
