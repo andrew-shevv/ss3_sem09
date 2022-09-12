65f799589df0ba5451947b68601c91235586a6b2
Andrew <vishnyaishere@gmail.com>
2022-09-12 20:13:58 +0300

    exc006 done

--- exc006_hw/Program.cs
+++ exc006_hw/Program.cs
@@ -4,3 +4,19 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+int SumBetween(int numberA, int numberB)
+{
+    if (numberA < numberB)
+    {
+        return numberA + SumBetween(numberA + 1, numberB);
+    }
+    else return numberA;
+}
+
+Console.Write("Input first(smaller) number: ");
+int numA = int.Parse(Console.ReadLine()!);
+
+Console.Write("Input second(bigger) number: ");
+int numB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(SumBetween(numA, numB));
